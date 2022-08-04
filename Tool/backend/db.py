634692e8aab0c60a181094dcdf0e8876bf312b3a
cihashev <cihashev99@gmail.com>
2022-08-05 01:16:54 +0200

    refactor: setup_db() moved to app.py

--- Tool/backend/db.py
+++ Tool/backend/db.py
@@ -2,25 +2,13 @@ from pathlib import Path
 import sqlite3
 from config import DB_PATH
 
-USERS_SCHEMA = """
+# email is needed for password recovery
+USERS_SCHEMA = '''
     CREATE TABLE users (
         id INTEGER PRIMARY KEY,
         username TEXT UNIQUE,
+        full_name TEXT,
         email TEXT,
         password TEXT
     )
-"""
-
-
-def setup_db():
-    # if database exists already, return
-    if Path(DB_PATH).exists():
-        return
-
-    conn = sqlite3.connect(DB_PATH)
-    cur = conn.cursor()
-
-    cur.execute(USERS_SCHEMA)
-
-    conn.commit()
-    conn.close()
+'''
