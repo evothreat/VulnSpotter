82332573884d0ae4cd8eb69afc2ccd4e00399977
cihashev <cihashev99@gmail.com>
2022-08-06 16:23:31 +0200

    feat: protected url sample

--- Tool/frontend/src/pages/Router.js
+++ Tool/frontend/src/pages/Router.js
@@ -1,11 +1,17 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./Login";
 import {NotFound} from "./NotFound";
+import RequireAuth from "./RequireAuth";
+
+
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route element={<RequireAuth/>}>
+                    <Route path="/" element={<h1>Accessing protected resource</h1>}/>
+                </Route>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Routes>
