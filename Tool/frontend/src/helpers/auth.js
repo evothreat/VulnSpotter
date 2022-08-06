afcba85ae4ef0cfd481491d3b3a1f7099b462a9a
cihashev <cihashev99@gmail.com>
2022-08-06 16:32:28 +0200

    feat: todos added

--- Tool/frontend/src/helpers/auth.js
+++ Tool/frontend/src/helpers/auth.js
@@ -1,5 +1,8 @@
 import AuthAPI from "../apis/auth";
 
+// TODO: make every n-minutes api request to verify authentication
+// TODO: set isLoggedIn=false if any api request finishes with error code 401
+
 export default class AuthHelper {
     static login(username, password) {
         return AuthAPI.login(username, password)
