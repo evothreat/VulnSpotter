dc09de46ac727a5f8ebf17739172df83d4d5746f
cihashev <cihashev99@gmail.com>
2022-08-10 18:23:07 +0200

    feat: members schema refactor: field 'accessed' to 'updated'

--- Tool/backend/db.py
+++ Tool/backend/db.py
@@ -1,8 +1,5 @@
-from pathlib import Path
-import sqlite3
-from config import DB_PATH
-
 # email is needed for password recovery
+# check for username uniqueness when registering?
 USERS_SCHEMA = '''
     CREATE TABLE users (
         id          INTEGER PRIMARY KEY,
@@ -15,10 +12,21 @@ USERS_SCHEMA = '''
 
 PROJECTS_SCHEMA = '''
     CREATE TABLE projects (
-        id          INTEGER PRIMARY KEY,
-        name        TEXT,
-        owner       INTEGER,
-        accessed    DATETIME,
+        id      INTEGER PRIMARY KEY,
+        name    TEXT,
+        owner   INTEGER,
+        updated DATETIME,
         FOREIGN KEY(owner) REFERENCES users(id)
+    )
+'''
+
+MEMBERS_SCHEMA = '''
+    CREATE TABLE members (
+        user_id     INTEGER,
+        project_id  INTEGER,
+        role        TEXT,
+        FOREIGN KEY(user_id) REFERENCES users(id),
+        FOREIGN KEY(project_id) REFERENCES projects(id),
+        PRIMARY KEY(user_id, project_id)
     )
 '''
