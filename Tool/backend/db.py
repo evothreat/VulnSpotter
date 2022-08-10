e210879ae9b97d2ff2c6ca35c1a8a8ec0882b83f
cihashev <cihashev99@gmail.com>
2022-08-10 11:55:55 +0200

    feat: projects schema

--- Tool/backend/db.py
+++ Tool/backend/db.py
@@ -5,10 +5,20 @@ from config import DB_PATH
 # email is needed for password recovery
 USERS_SCHEMA = '''
     CREATE TABLE users (
-        id INTEGER PRIMARY KEY,
-        username TEXT UNIQUE,
-        full_name TEXT,
-        email TEXT,
-        password TEXT
+        id          INTEGER PRIMARY KEY,
+        username    TEXT UNIQUE,
+        full_name   TEXT,
+        email       TEXT,
+        password    TEXT
+    )
+'''
+
+PROJECTS_SCHEMA = '''
+    CREATE TABLE projects (
+        id          INTEGER PRIMARY KEY,
+        name        TEXT,
+        owner       INTEGER,
+        accessed    DATETIME,
+        FOREIGN KEY(owner) REFERENCES users(id)
     )
 '''
