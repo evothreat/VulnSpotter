e09daa06ab5ad8142af60b10db52008be29060ab
cihashev <cihashev99@gmail.com>
2022-08-10 21:47:04 +0200

    feat: retrieve projects list & show time since now in human-readable format

--- Tool/frontend/src/pages/Repositories/RepositoryTable.js
+++ Tool/frontend/src/pages/Repositories/RepositoryTable.js
@@ -7,36 +7,35 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import IconButton from "@mui/material/IconButton";
-import {TableSortLabel} from "@mui/material";
-import {useState} from "react";
+import {CircularProgress, TableSortLabel} from "@mui/material";
+import {useEffect, useState} from "react";
+import ProjectsApi from "../../apis/projectsApi";
+import AuthUtil from "../../utils/authUtil";
+import TimeUtil from "../../utils/timeUtil";
 
-function createData(name, owner, accessed) {
-    return {name, owner, accessed};
-}
 
-const rows = [
-    createData('camino', 'Me', '3 hours ago'),
-    createData('chatzilla', 'John Rambo', '1 minute ago'),
-    createData('penelope', 'Bruce Campbell', '45 seconds ago'),
-    createData('mobile-browser', 'Me', '24 days ago'),
-    createData('graphs', 'Me', '16 hours ago'),
-    createData('dom-inspector', 'Ash Williams', '6 years ago'),
-    createData('cvs-trunk-mirror', 'Christopher Nolan', '5 weeks ago'),
-    createData('comm-central', 'Jackie Chan', '7 months ago'),
-    createData('pyxpcom', 'Me', '9 months ago'),
-    createData('schema-validation', 'Jean Claude Van Damme', '2 years ago'),
-    createData('tamarin-redux', 'Nicolas Cage', '35 minutes ago'),
-    createData('venkman', 'Leonardo Di Caprio', '2 hours ago'),
-];
 
+// TODO: rename everything with 'Repository' to 'Project'
 export default function RepositoryTable() {
 
-    const [order, setOrder] = useState('');             // TODO: add default sort by access time
-    const [orderBy, setOrderBy] = useState('');
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        ProjectsApi.getProjects()
+            .then((resp) => {
+                setProjects(resp.data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                if (err.response.status < 500) AuthUtil.invalidate();
+                console.log(err);
+            });
+    }, []);
 
     return (
         <TableContainer sx={{maxHeight: 500}}>
-            <Table size='small' stickyHeader>
+            <Table size="small" stickyHeader>
                 <TableHead>
                     <TableRow>
                         <TableCell sx={{fontWeight: 'bold', width: '40%'}}>
@@ -46,27 +45,29 @@ export default function RepositoryTable() {
                             <TableSortLabel>Owner</TableSortLabel>
                         </TableCell>
                         <TableCell sx={{fontWeight: 'bold', width: '20%'}}>
-                            <TableSortLabel>Accessed</TableSortLabel>
+                            <TableSortLabel>Updated</TableSortLabel>
                         </TableCell>
                         <TableCell sx={{width: '5%'}}/>
                     </TableRow>
                 </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow>
-                            <TableCell>
-                                {row.name}
-                            </TableCell>
-                            <TableCell>{row.owner}</TableCell>
-                            <TableCell>{row.accessed}</TableCell>
-                            <TableCell>
-                                <IconButton>
-                                    <MoreVertIcon/>
-                                </IconButton>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
+                {loading ? <CircularProgress/> :
+                    <TableBody> {
+                        projects.map((p) => (
+                            <TableRow key={p.id}>
+                                <TableCell>
+                                    {p.name}
+                                </TableCell>
+                                <TableCell>{p.owner.full_name}</TableCell>
+                                <TableCell>{TimeUtil.since(new Date(p.updated)) + ' ago'}</TableCell>
+                                <TableCell>
+                                    <IconButton>
+                                        <MoreVertIcon/>
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                }
             </Table>
         </TableContainer>
     );
