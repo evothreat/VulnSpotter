35a5fa13edeccb15a30ba7316ea4e334bf523bb7
cihashev <cihashev99@gmail.com>
2022-08-14 19:58:55 +0200

    feat: sort items in project-table

--- Tool/frontend/src/pages/Projects/ProjectTable.js
+++ Tool/frontend/src/pages/Projects/ProjectTable.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -13,6 +13,13 @@ import ProjectsService from "../../services/ProjectsService";
 import TimeUtil from "../../utils/TimeUtil";
 import AuthService from "../../services/AuthService";
 
+
+function createComparator(key, order) {
+    if (order === 'asc') {
+        return (a, b) => a[key] > b[key] ? 1 : -1;
+    }
+    return (a, b) => a[key] < b[key] ? 1 : -1;
+}
 
 function applyGroupFilter(items, group) {
     const userId = AuthService.getCurrentUser().id;
@@ -28,9 +35,15 @@ export default function ProjectTable(props) {
     const [items, setItems] = useState([]);
     const [itemsLoaded, setItemsLoaded] = useState(false);
 
+    const sorterRef = useRef({key: '', order: ''});
+    const sorter = sorterRef.current;
+
     useEffect(() => {
         ProjectsService.getProjects()
             .then((resp) => {
+                resp.data.forEach((p) => {
+                    p.owner_name = p.owner.full_name;
+                });
                 setItems(resp.data);
                 setItemsLoaded(true);
             })
@@ -54,7 +67,7 @@ export default function ProjectTable(props) {
                 rows.push(
                     <TableRow key={p.id}>
                         <TableCell>{p.name}</TableCell>
-                        <TableCell>{p.owner.full_name}</TableCell>
+                        <TableCell>{p.owner_name}</TableCell>
                         <TableCell>{TimeUtil.since(new Date(p.last_update)) + ' ago'}</TableCell>
                         <TableCell>
                             <IconButton>
@@ -70,19 +83,39 @@ export default function ProjectTable(props) {
             <caption>There are no items to display.</caption>;
     };
 
+    const createHeadCell = (key, name, width) => {
+        return (
+            <TableCell
+                sx={{ fontWeight: 'bold', width: width }}
+                sortDirection={sorter.key === key ? sorter.order : false}
+            >
+                <TableSortLabel
+                    active={sorter.key === key}
+                    direction={sorter.key === key ? sorter.order : 'asc'}
+                    data-key={key}
+                    onClick={reorderItems}
+                >
+                    {name}
+                </TableSortLabel>
+            </TableCell>
+        );
+    }
+
+    const reorderItems = (e) => {
+        sorter.key = e.currentTarget.dataset.key;
+        sorter.order = sorter.order === 'asc' ? 'desc' : 'asc'
+
+        const ordered = items.slice().sort(createComparator(sorter.key, sorter.order));
+        setItems(ordered);
+    }
+
     return (<TableContainer sx={{maxHeight: 500}}>
             <Table size="small" stickyHeader>
                 <TableHead>
                     <TableRow>
-                        <TableCell sx={{fontWeight: 'bold', width: '40%'}}>
-                            <TableSortLabel>Name</TableSortLabel>
-                        </TableCell>
-                        <TableCell sx={{fontWeight: 'bold', width: '35%'}}>
-                            <TableSortLabel>Owner</TableSortLabel>
-                        </TableCell>
-                        <TableCell sx={{fontWeight: 'bold', width: '20%'}}>
-                            <TableSortLabel>Updated</TableSortLabel>
-                        </TableCell>
+                        {createHeadCell('name', 'Name', '40%')}
+                        {createHeadCell('owner_name', 'Owner', '35%')}
+                        {createHeadCell('last_update', 'Updated', '20%')}
                         <TableCell sx={{width: '5%'}}/>
                     </TableRow>
                 </TableHead>
