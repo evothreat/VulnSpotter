c23e619c7f6632b591d089337abd0ee5eef37e62
cihashev <cihashev99@gmail.com>
2022-09-23 00:53:11 +0200

    refactor: retrieve user data from separate endpoint

--- Tool/frontend/src/services/AuthService.js
+++ Tool/frontend/src/services/AuthService.js
@@ -8,13 +8,17 @@ class AuthService {
             username: username,
             password: password,
         }).then((resp) => {
-            const data = resp.data;
-            if (data.user) {
-                TokenService.setRefreshToken(data.refresh_token);
-                TokenService.setAccessToken(data.access_token);
-                TokenService.setIdentity(data.user);
-                return data;
-            }
+            TokenService.setRefreshToken(resp.data.refresh_token);
+            TokenService.setAccessToken(resp.data.access_token);
+
+            return api.get('/users/me')
+                .then((r) => {
+                    TokenService.setIdentity(r.data);
+                })
+                .catch((err) => {
+                    TokenService.invalidate();
+                    return Promise.reject(err);
+                });
         });
     }
 
