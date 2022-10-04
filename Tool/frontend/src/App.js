a9c51e4cdcd56409cf904e3508846deaa4349f2b
cihashev <cihashev99@gmail.com>
2022-10-05 00:37:42 +0200

    refactor: unnecessary files & code lines removed; Router-component moved to App.js

--- Tool/frontend/src/App.js
+++ Tool/frontend/src/App.js
@@ -1,8 +1,28 @@
-import './App.css';
-import Router from "./Router";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import RequireAuth from "./RequireAuth";
+import {Fragment} from "react";
+import Header from "./layout/Header";
+import {Projects} from "./pages/Projects/Projects";
+import Login from "./pages/Login/Login";
+import {NotFound} from "./pages/NotFound";
 
 function App() {
-    return <Router/>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<RequireAuth/>}>
+                    <Route path="/projects" element={
+                        <Fragment>
+                            <Header/>
+                            <Projects/>
+                        </Fragment>
+                    }/>
+                </Route>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
