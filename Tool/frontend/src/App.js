091c9305f2dfe94ee3a1983dbe2d17694bd4a8b4
cihashev <cihashev99@gmail.com>
2022-10-05 02:55:49 +0200

    refactor: RequireAuth component moved to App.js

--- Tool/frontend/src/App.js
+++ Tool/frontend/src/App.js
@@ -1,10 +1,18 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import RequireAuth from "./RequireAuth";
+import {BrowserRouter, Navigate, Outlet, Route, Routes} from "react-router-dom";
 import {Fragment} from "react";
 import Header from "./layout/Header";
 import {Projects} from "./pages/Projects/Projects";
 import Login from "./pages/Login/Login";
 import {NotFound} from "./pages/NotFound";
+import AuthService from "./services/AuthService";
+
+
+function RequireAuth() {
+    if (!AuthService.isLoggedIn()) {
+        return <Navigate to="/login"/>;
+    }
+    return <Outlet/>;
+}
 
 function App() {
     return (
