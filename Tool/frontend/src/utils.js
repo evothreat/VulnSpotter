19ee87fcfa032b39e8e8a94894d4b2543426f9b5
cihashev <cihashev99@gmail.com>
2022-10-24 02:44:17 +0200

    feat: calculate side-by-side diff

--- Tool/frontend/src/utils.js
+++ Tool/frontend/src/utils.js
@@ -1,3 +1,5 @@
+import * as DiffLib from "diff";
+
 function fmtTimeSince(time) {
     const date = typeof time === 'number' ? new Date(time * 1000) : time;
     const seconds = Math.floor((new Date() - date) / 1000);
@@ -53,6 +55,78 @@ function remove(a, id) {
     return a.filter((v) => v.id !== id);
 }
 
+// to compute side-by-side diff
+function calcSplitDiff(oldCode, newCode) {
+    const lineDiff = DiffLib.diffLines(oldCode, newCode);
+    const diff = [];
+    let ix = 0;
+    while (lineDiff.length > ix) {
+        const {removed, added, value, count} = lineDiff[ix];
+        const lines = value.split('\n', count);
+        if (removed) {
+            if (lineDiff[ix+1].added) {
+                const next = lineDiff[ix+1];
+                const nextLines = next.value.split('\n', next.count);
+
+                const maxN = Math.max(count, next.count);   // use min & add rest after?
+                for (let i = 0; maxN > i; i++) {
+                    diff.push({left: lines[i], right: nextLines[i]});
+                }
+                ix++;
+            } else {
+                lines.forEach((l) => diff.push({left: l}));
+            }
+        }
+        else if (added) {
+            lines.forEach((l) => diff.push({right: l}));
+        }
+        else {
+            lines.forEach((l) => diff.push({neutral: l}));
+        }
+        ix++;
+    }
+    return diff;
+}
+
+function calcSplitDiffCompact(oldCode, newCode) {
+    const lineDiff = DiffLib.diffLines(oldCode, newCode);
+    const diff = [];
+    let ix = 0;
+    while (lineDiff.length > ix) {
+        const {removed, added, value, count} = lineDiff[ix];
+        if (removed) {
+            if (lineDiff[ix+1].added) {
+                const lines = value.split('\n');
+
+                const next = lineDiff[ix+1];
+                const nextLines = next.value.split('\n');
+
+                const minN = Math.min(count, next.count);
+                for (let i = 0; minN > i; i++) {
+                    diff.push({left: lines[i], right: nextLines[i]});
+                }
+                if (count > minN) {
+                    diff.push({left: lines.slice(minN).join('\n')});
+                }
+                if (next.count > minN) {
+                    diff.push({right: nextLines.slice(minN).join('\n')});
+                }
+                ix++;
+            } else {
+                diff.push({left: value});
+            }
+        }
+        else if (added) {
+            diff.push({right: value});
+        }
+        else {
+            diff.push({neutral: value});
+        }
+        ix++;
+    }
+    return diff;
+}
+
 export {
     fmtTimeSince,
     createComparator,
@@ -60,5 +134,7 @@ export {
     findCVEs,
     complement,
     equals,
-    remove
+    remove,
+    calcSplitDiff,
+    calcSplitDiffCompact
 };
