2f56489a9572008b6b91797356dbda5c0accb07d
cihashev <cihashev99@gmail.com>
2022-11-02 18:31:14 +0100

    fix: calling execute/executemany inside context caused permanent locking

--- Tool/backend/safe_sql.py
+++ Tool/backend/safe_sql.py
@@ -6,23 +6,32 @@ class SafeSql(sqlite3.Connection):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.lock = Lock()
+        self._transaction = False
 
     def __enter__(self):
         self.lock.acquire()
+        self._transaction = True
         super().__enter__()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        self._transaction = False
         self.lock.release()
         super().__exit__(exc_type, exc_val, exc_tb)
 
-    def execute(self, sql, parameters=()):
+    def execute(self, *args, **kwargs):
+        if self._transaction:
+            return super().execute(*args, **kwargs)
+
         self.lock.acquire()
-        cur = super().execute(sql, parameters)
+        cur = super().execute(*args, **kwargs)
         self.lock.release()
         return cur
 
-    def executemany(self, sql, parameters):
+    def executemany(self, *args, **kwargs):
+        if self._transaction:
+            return super().execute(*args, **kwargs)
+
         self.lock.acquire()
-        cur = super().execute(sql, parameters)
+        cur = super().execute(*args, **kwargs)
         self.lock.release()
         return cur
