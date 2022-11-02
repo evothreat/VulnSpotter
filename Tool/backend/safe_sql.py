4a41ead812c7d83dd1c79c6701ef9bd5c7bcbd41
cihashev <cihashev99@gmail.com>
2022-11-02 18:35:14 +0100

    refactor: lock visibility limited to private

--- Tool/backend/safe_sql.py
+++ Tool/backend/safe_sql.py
@@ -5,33 +5,33 @@ from threading import Lock
 class SafeSql(sqlite3.Connection):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.lock = Lock()
+        self._lock = Lock()
         self._transaction = False
 
     def __enter__(self):
-        self.lock.acquire()
+        self._lock.acquire()
         self._transaction = True
         super().__enter__()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._transaction = False
-        self.lock.release()
+        self._lock.release()
         super().__exit__(exc_type, exc_val, exc_tb)
 
     def execute(self, *args, **kwargs):
         if self._transaction:
             return super().execute(*args, **kwargs)
 
-        self.lock.acquire()
+        self._lock.acquire()
         cur = super().execute(*args, **kwargs)
-        self.lock.release()
+        self._lock.release()
         return cur
 
     def executemany(self, *args, **kwargs):
         if self._transaction:
             return super().execute(*args, **kwargs)
 
-        self.lock.acquire()
+        self._lock.acquire()
         cur = super().execute(*args, **kwargs)
-        self.lock.release()
+        self._lock.release()
         return cur
