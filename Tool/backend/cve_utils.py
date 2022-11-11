6e861f17666ac761954ed7e4c1c002f8a1679936
cihashev <cihashev99@gmail.com>
2022-11-11 17:10:19 +0100

    feat: improved cve-parsing
    removing dashes & underscores from package-name and 'keywordExactMatch' from query string, increases chance of identifying cve (the description is checked for existence of all words)

--- Tool/backend/cve_utils.py
+++ Tool/backend/cve_utils.py
@@ -3,19 +3,21 @@ from time import sleep
 import re
 
 
-CVE_DETAILS_URL = 'https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch={}&keywordExactMatch&startIndex={}'
-CVE_SUMMARY_URL = 'https://access.redhat.com/hydra/rest/securitydata/cve.json?ids={}'
+CVE_DETAILS_URL = 'https://services.nvd.nist.gov/rest/json/cves/2.0'
+CVE_SUMMARY_URL = 'https://access.redhat.com/hydra/rest/securitydata/cve.json'
 
 
 def get_cve_details(hint, cve_ids, max_tries=3, start_index=0):
     if not (hint and cve_ids):
         return {}
 
-    resp = reqs.get(CVE_DETAILS_URL.format(hint, start_index))
+    resp = reqs.get(CVE_DETAILS_URL, params={'keywordSearch': hint, 'startIndex': start_index})
     if resp.status_code != 200:
-        if max_tries > 0:
-            sleep(1)
+        # we do not check for 500 error codes, cause sometimes response has 403
+        if resp.status_code != 404 and max_tries > 0:
+            sleep(3)
             return get_cve_details(hint, cve_ids, max_tries - 1, start_index)
+
         return {}
 
     data = resp.json()
@@ -44,8 +46,10 @@ def get_cve_details(hint, cve_ids, max_tries=3, start_index=0):
 
     total_seen = start_index + data['resultsPerPage']
     if data['totalResults'] > total_seen:
+        # sleep to prevent from being banned
+        sleep(data['totalResults'] / data['resultsPerPage'])
         res.update(
-            get_cve_details(hint, {cid for cid in cve_ids if cid not in res}, start_index=total_seen)
+            get_cve_details(hint, {k for k in cve_ids if k not in res}, start_index=total_seen)
         )
     return res
 
@@ -54,10 +58,10 @@ def get_cve_summary(cve_ids, max_tries=3, start_index=0):
     if not cve_ids:
         return {}
 
-    resp = reqs.get(CVE_SUMMARY_URL.format(','.join(cve_ids[start_index:100])))
+    resp = reqs.get(CVE_SUMMARY_URL, params={'ids': cve_ids[start_index:100]})
     if resp.status_code != 200:
         if max_tries > 0:
-            sleep(1)
+            sleep(3)
             return get_cve_summary(cve_ids, max_tries - 1, start_index)
         return {}
 
@@ -82,7 +86,8 @@ def get_cve_summary(cve_ids, max_tries=3, start_index=0):
 
 
 def get_cve_info(hint, cve_ids):
-    cve_info = get_cve_details(hint, cve_ids)
+    cve_info = get_cve_details(hint.replace('-', ' ').replace('_', ' '), cve_ids)
+
     if not cve_info:
         return {}
 
