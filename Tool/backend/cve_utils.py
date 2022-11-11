63be9b8671b1cfe2feaa705f25ecb74f63d50c7f
cihashev <cihashev99@gmail.com>
2022-11-11 22:04:13 +0100

    feat: parse the rest of cves, which wasn't found on first try

--- Tool/backend/cve_utils.py
+++ Tool/backend/cve_utils.py
@@ -2,16 +2,26 @@ import requests as reqs
 from time import sleep
 import re
 
+CVE_API_NVD = 'https://services.nvd.nist.gov/rest/json/cves/2.0'
+CVE_API_REDHAT = 'https://access.redhat.com/hydra/rest/securitydata/cve.json'
 
-CVE_DETAILS_URL = 'https://services.nvd.nist.gov/rest/json/cves/2.0'
-CVE_SUMMARY_URL = 'https://access.redhat.com/hydra/rest/securitydata/cve.json'
+MAX_REQUESTED = 100
+MAX_PARSED_ALONE = 10
+
+
+def strip_alias(s):
+    if re.match('cve', s, re.I):
+        delim_ix = s.find(':')
+        if delim_ix > -1:
+            return s[delim_ix + 1:].lstrip()
+    return s
 
 
 def get_cve_details(hint, cve_ids, max_tries=3, start_index=0):
     if not (hint and cve_ids):
         return {}
 
-    resp = reqs.get(CVE_DETAILS_URL, params={'keywordSearch': hint, 'startIndex': start_index})
+    resp = reqs.get(CVE_API_NVD, params={'keywordSearch': hint, 'startIndex': start_index})
     if resp.status_code != 200:
         # we do not check for 500 error codes, cause sometimes response has 403
         if resp.status_code != 404 and max_tries > 0:
@@ -24,12 +34,11 @@ def get_cve_details(hint, cve_ids, max_tries=3, start_index=0):
     if data['totalResults'] == 0:
         return {}
 
-    cve_lookup = cve_ids if isinstance(cve_ids, set) else set(cve_ids)
     res = {}
     for v in data['vulnerabilities']:
         cve = v['cve']
         cve_id = cve['id']
-        if cve_id not in cve_lookup:
+        if cve_id not in cve_ids:
             continue
 
         description = cve['descriptions'][0]['value']
@@ -49,7 +58,7 @@ def get_cve_details(hint, cve_ids, max_tries=3, start_index=0):
         # sleep to prevent from being banned
         sleep(data['totalResults'] / data['resultsPerPage'])
         res.update(
-            get_cve_details(hint, {k for k in cve_ids if k not in res}, start_index=total_seen)
+            get_cve_details(hint, cve_ids - res.keys(), start_index=total_seen)
         )
     return res
 
@@ -58,7 +67,7 @@ def get_cve_summary(cve_ids, max_tries=3, start_index=0):
     if not cve_ids:
         return {}
 
-    resp = reqs.get(CVE_SUMMARY_URL, params={'ids': cve_ids[start_index:100]})
+    resp = reqs.get(CVE_API_REDHAT, params={'ids': cve_ids[start_index:MAX_REQUESTED]})
     if resp.status_code != 200:
         if max_tries > 0:
             sleep(3)
@@ -71,22 +80,49 @@ def get_cve_summary(cve_ids, max_tries=3, start_index=0):
         if not summary:
             continue
 
-        if re.match('cve', summary, re.I):
-            delim_ix = summary.find(':')
-            if delim_ix > -1:
-                summary = summary[delim_ix + 1:].lstrip()
+        res[v['CVE']] = strip_alias(summary)
 
-        res[v['CVE']] = summary
-
-    total_parsed = start_index + 100
+    total_parsed = start_index + MAX_REQUESTED
     if len(cve_ids) > total_parsed:
+        sleep(0.5)
         res.update(get_cve_summary(cve_ids, start_index=total_parsed))
 
     return res
 
 
+def get_cve_details_redhat(cve_ids):
+    cve_url = CVE_API_REDHAT.rstrip('.json')
+    res = {}
+    for cid in cve_ids:
+        resp = reqs.get(f'{cve_url}/{cid}.json')
+        if resp.status_code != 200:
+            continue  # ignore errors
+
+        data = resp.json()
+        description = data['details'][0]
+
+        bugzilla = data.get('bugzilla')
+        summary = strip_alias(bugzilla['description']) if bugzilla else None
+
+        cvss = data.get('cvss3', data.get('cvss'))
+        score = float(next(v for k, v in cvss.items() if k.endswith('_base_score'))) if cvss else None
+
+        res[cid] = {
+            'summary': summary,
+            'description': description,
+            'score': score
+        }
+        sleep(0.5)
+
+    return res
+
+
 def get_cve_info(hint, cve_ids):
+    cve_ids = cve_ids if isinstance(cve_ids, set) else set(cve_ids)
     cve_info = get_cve_details(hint.replace('-', ' ').replace('_', ' '), cve_ids)
+
+    if MAX_PARSED_ALONE >= len(cve_ids) - len(cve_info):
+        cve_info = get_cve_details_redhat(cve_ids - cve_info.keys())
 
     if not cve_info:
         return {}
