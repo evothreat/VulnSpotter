22ca0f980ebfb7cd4c39323624a06f889be71eb9
cihashev <cihashev99@gmail.com>
2023-04-30 14:51:16 +0200

    style: Software-Name changed

--- Tool/frontend/src/layout/LayoutHeader.js
+++ Tool/frontend/src/layout/LayoutHeader.js
@@ -79,7 +79,7 @@ export default function LayoutHeader() {
                                     textDecoration: 'none',
                                 }}
                     >
-                        VulnDetector
+                        VulnTector
                     </RouterLink>
                 </Box>
                 <Box>
